4a42100da198f54e43608f027eaf7ce394fc0914
Dhiraj <starkjon224@gmail.com>
2023-12-09 23:44:10 +0545

    updating

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,19 @@
 import 'package:coincap/pages/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await loadConfig();
   runApp(const MyApp());
+}
+
+Future<void> loadConfig() async {
+  String _configContent =
+      await rootBundle.loadString("assets/config/main.json");
+  Map _configData = jsonDecode(_configContent);
+  print(_configData);
 }
 
 class MyApp extends StatelessWidget {
