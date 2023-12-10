d41183f5ce0d17fa9f15bd46ee1656996ca23aeb
Dhiraj <starkjon224@gmail.com>
2023-12-10 22:28:36 +0545

    adding http service

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:coincap/models/app_config.dart';
 import 'package:coincap/pages/home_screen.dart';
+import 'package:coincap/services/http_service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'dart:convert';
+
+import 'package:get_it/get_it.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -13,7 +17,18 @@ Future<void> loadConfig() async {
   String _configContent =
       await rootBundle.loadString("assets/config/main.json");
   Map _configData = jsonDecode(_configContent);
-  print(_configData);
+  // print(_configData);
+  GetIt.instance.registerSingleton<AppConfig>(
+    AppConfig(
+      COIN_API_BASE_URL: _configData["COIN_API_BASE_URL"],
+    ),
+  );
+}
+
+void registerHTTPService() {
+  GetIt.instance.registerSingleton<HTTPService>(
+    HTTPService(),
+  );
 }
 
 class MyApp extends StatelessWidget {
