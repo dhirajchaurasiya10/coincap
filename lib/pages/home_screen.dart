272aaef22f330b1a69a10ffeb522e0ee4c392f9d
Dhiraj <starkjon224@gmail.com>
2023-12-12 23:39:02 +0545

    fetching from api

--- lib/pages/home_screen.dart
+++ lib/pages/home_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:coincap/services/http_service.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
@@ -33,6 +35,7 @@ class _HomePageState extends State<HomePage> {
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             _selectedCoinDropdown(),
+            _datawidget(),
           ],
         ),
       )),
@@ -61,11 +64,51 @@ class _HomePageState extends State<HomePage> {
       onChanged: (_value) {},
       dropdownColor: Color.fromRGBO(83, 88, 206, 1.0),
       iconSize: 30,
-      icon: Icon(
+      icon: const Icon(
         Icons.arrow_drop_down_sharp,
         color: Colors.white,
       ),
       underline: Container(), //to remove the underline of the drop down button
+    );
+  }
+
+  // Here is the use of Circular Progress Indicator which can be used for future works
+  Widget _datawidget() {
+    return FutureBuilder(
+      future: _http!.get("/coins/bitcoin"),
+      builder: (BuildContext _context, AsyncSnapshot _snapshot) {
+        if (_snapshot.hasData) {
+          Map _data = jsonDecode(
+            _snapshot.data.toString(),
+          );
+          num _usdPrice = _data["market_data"]["current_price"]["usd"];
+          return Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              _currentPriceWidget(_usdPrice),
+            ],
+          );
+        } else {
+          return const Center(
+            child: CircularProgressIndicator(
+              color: Colors.white,
+            ),
+          );
+        }
+      },
+    );
+  }
+
+  Widget _currentPriceWidget(num _rate) {
+    return Text(
+      "${_rate.toStringAsFixed(2)} USD",
+      style: TextStyle(
+        color: Colors.white,
+        fontSize: 30,
+        fontWeight: FontWeight.w300,
+      ),
     );
   }
 }
